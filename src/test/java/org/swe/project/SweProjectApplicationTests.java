bdd877249393899c7b7db64f400c03a0d696489c
JM4128 <Joshmiller8800@gmail.com>
2024-02-07 20:02:38 -0500

    init commit with spring dependencies for web

---
+++ src/test/java/org/swe/project/SweProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swe.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
